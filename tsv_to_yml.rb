3a4f11cc33338c27ceed8ea913bb6e088ea2585a
eslywadan <esly.wadan@gmail.com>
2020-10-11 12:38:17 +0800

    Refactoring (1) Use 'begin..end' (2) use named constants

--- tsv_to_yml.rb
+++ tsv_to_yml.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
 # The rb code will accept the user's tsv input and convert it to yaml output.
+# Refactoring 1. Use begin..end transaction on processing steps 2.Use Named constants instead of having "magic constants"
 require 'yaml'
 require 'csv'
 
+TAB = "\t"
+
 def load_tsv(in_tsv_file)
   tsv_data = []
-  tsv_file = CSV.open(in_tsv_file, 'r', **{ col_sep: "\t" })
+  tsv_file = CSV.open(in_tsv_file, 'r', **{ col_sep: TAB })
   tsv_file.each do |row|
     tsv_data.append(row)
   end
@@ -24,6 +27,8 @@ def write_yaml(yaml_data, out_yml_file)
   File.write(out_yml_file, yaml_data) unless out_yml_file.nil?
 end
 
-tsv_data = load_tsv(ARGV[0])
-yaml_data = convert_to_yaml(tsv_data)
-write_yaml(yaml_data, ARGV[1])
+begin
+  tsv_data = load_tsv(ARGV[0])
+  yaml_data = convert_to_yaml(tsv_data)
+  write_yaml(yaml_data, ARGV[1])
+end
